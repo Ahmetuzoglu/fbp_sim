ed437f30946e08424a117f4588aefa366c938802
Ahmetuzoglu <Ahmetuzoglu25@gmail.com>
2026-02-27 22:35:42 +0300

    Python_Analysis

---
+++ build/CMakeFiles/scan.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scan.dir/src/main.cpp.obj"
+  "CMakeFiles/scan.dir/src/main.cpp.obj.d"
+  "libscan.dll.a"
+  "scan.exe"
+  "scan.exe.manifest"
+  "scan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
